577fa1c3e7038f7c69da06b22446044bff392b71
Kvillalobos210 <73324238+Kvillalobos210@users.noreply.github.com>
2021-11-26 00:22:57 -0500

    v1

---
+++ Presentación Final/BruteForce.py
@@ -0,0 +1,39 @@
+def bruteforce(G, s):
+  n = len(G)
+  tvisit = [None, s]
+  parent = []
+  visited = [False]*n
+  while(tvisit):
+    v = tvisit.pop()
+    if v : 
+      parent.append(v)
+      if len(parent) == n:
+        break
+      visited[v]
+      for x in G[v]:
+        tvisit.append(None)
+        tvisit.append(x) 
+    else: 
+      visited.remove(parent.pop())
+  return parent
+
+#Algoritmo Brute Force punto a punto 
+
+def bfPointToPoint(G,s,t):
+  n = len(G)
+  tvisit = [None, s]
+  parent = []
+  visited = [False]*n
+  while(tvisit):
+    v = tvisit.pop()
+    if v : 
+      parent.append(v)
+      if len(parent) == n:
+        break
+      visited[v]
+      for x in G[v]:
+        tvisit.append(None)
+        tvisit.append(x) 
+    else: 
+      visited.remove(parent.pop())
+  return parent, parent[t]
